96f9c14125426f830bb57b7c88cdb8486919b7d2
fadilxmalakmal <maulanaakma7@gmail.com>
2025-08-08 13:28:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="akmal",
+    page_icon=":tada:",
+    layout="wide"
+    )
+
+st.sidebar.succes("About Me")
 st.title("Website ini dibuat oleh akmal")
